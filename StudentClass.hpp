2d1f562196806162d293cfe0ce3d8ef07a8cf433
Marble <scnideffer@gmail.com>
2024-05-12 20:52:50 -0700

    Added student class to spec, I hope

--- StudentClass.hpp
+++ StudentClass.hpp
@@ -5,4 +5,34 @@
 #ifndef DYNAMICCLASSROOM_STUDENTCLASS_HPP
 #define DYNAMICCLASSROOM_STUDENTCLASS_HPP
 
+#include <iostream>
+
+class Student {
+private:
+    std::string name;
+    float GPA;
+public:
+    // Constructors
+    Student();
+    Student(std::string, float);
+    // Deconstructor
+    ~Student();
+
+
+};
+
+Student::Student(){
+    this->name = "";
+    this->GPA = 0;
+}
+
+Student::Student(std::string name, float GPA){
+    this->name = name;
+    this->GPA = GPA;
+}
+
+Student::~Student(){
+
+}
+
 #endif //DYNAMICCLASSROOM_STUDENTCLASS_HPP
